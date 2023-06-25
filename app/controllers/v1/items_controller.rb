3c60e37f698c77ea2dddb2de800078e271b32d7e
V0pe <dampblew01@gmail.com>
2023-06-25 16:42:27 -0600

    Serialize the items json endpoint

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -1,23 +1,25 @@
 class V1::ItemsController < ApplicationController
   def index
-    render json: Item.all.to_json
+    @item = Item.all
+
+    render json: ItemSerializer.new(@item).serializable_hash[:data], status: :ok
   end
 
   def show
-    item = Item.find_by(id: params[:id])
+    @item = Item.find_by(id: params[:id])
 
-    if item.nil?
+    if @item.nil?
       render status: 404, json: { error: 'item not found' }.to_json
     else
-      render json: item.to_json
+      render json: ItemSerializer.new(@item).serializable_hash[:data][:attributes], status: :ok
     end
   end
 
   def create
-    item = Item.new(item_params)
+    @item = Item.new(item_params)
 
-    if item.save
-      render json: item.to_json
+    if @item.save
+      render json: ItemSerializer.new(@item).serializable_hash[:data][:attributes], status: :created
     else
       render status: 500, json: { error: 'item could not be created' }.to_json
     end
@@ -37,6 +39,6 @@ class V1::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :color, :price, :brand, :category)
+    params.require(:item).permit(:name, :color, :price, :brand, :category, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,14 @@
 class Item < ApplicationRecord
   has_many :orders, dependent: :destroy
+  has_one_attached :image
 
   validates :name, presence: true
   validates :color, presence: true
   validates :price, presence: true
   validates :brand, presence: true
   validates :category, presence: true
+
+  def image_url
+    Rails.application.routes.url_helpers.url_for(image) if image.attached?
+  end
 end
