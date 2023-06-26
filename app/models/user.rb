fa05ce3a5cb05c2e9ee79ef904b8e11eba25c062
V0pe <dampblew01@gmail.com>
2023-06-25 21:35:21 -0600

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :first_name, uniqueness: { case_sensitive: false }, presence: true
   validates :last_name, uniqueness: { case_sensitive: false }
   validates :country, presence: true
